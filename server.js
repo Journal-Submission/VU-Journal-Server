4203eac7068faf64cfe12c353f46da51ed6a1a13
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2024-12-14 22:40:06 +0530

    added helmet for limitation use of api

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ const app = express();
 const cors = require('cors');
 const postRoute = require('./routes/postRoute');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 require('./database/db');
+
+app.use(helmet());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+});
+
+app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,11 +27,10 @@ app.use(cors({
 }));
 
 app.use('/', express.static('public'));
-
 app.use('/', postRoute);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Journal API');
+    res.send('Welcome to the Article Submission API');
 });
 
 // app.get('/save-cookie', (req, res) => {
